1ead0eecfea617e87d513177acb6ef30f53b6fb2
JacquesIM <jjamous@imfortrade.com>
2025-07-22 12:25:07 +0300

    Add Tax Rate Table module

---
+++ custom_addons/tax_rate_table/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Tax Rate Table',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Date-based Tax Rates like Currency Exchange',
+    'description': 'Manage variable tax rates by date and currency.',
+    'depends': ['account'],
+    'data': ['views/tax_rate_views.xml'],
+    'installable': True,
+}
